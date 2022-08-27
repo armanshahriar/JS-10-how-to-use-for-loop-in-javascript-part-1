9e60160737d6e3f66772a863a2fa2aaf1929be38
Arman Shahariar <111569490+armanshahriar@users.noreply.github.com>
2022-08-27 10:43:21 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+for (var x = 1; x <= 10; x = x + 1) {
+    document.write("<h1>Bangladesh</h1>")
+}
+document.write("<h1>End</h1>")
+
+for (var i = 1; i <= 10; i = i + 1) {
+    document.write("" + i + "<br>")
+}
+
+for (var i = 1; i <= 100; i = i + 1) {
+    document.write("" + i + "<br>")
+}
+
+for (var i = 1; i <= 10; i = i + 2) {
+    document.write("" + i + "<br>")
+}
